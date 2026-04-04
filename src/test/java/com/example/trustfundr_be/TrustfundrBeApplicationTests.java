62581d61076ada8f266aab2f3debc74688b7e1ab
Chelle007 <mc.chellechan@gmail.com>
2026-04-05 00:14:53 +0800

    first init

---
+++ src/test/java/com/example/trustfundr_be/TrustfundrBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trustfundr_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustfundrBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
